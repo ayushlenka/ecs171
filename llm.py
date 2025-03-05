d059039a41dbd1c717822837f5d12404b232e04f
yuonny <dvkim@ucdavis.edu>
2025-03-05 12:39:50 -0800

    llm.py outputs the emotion and market sentiment in an array

---
+++ llm.py
@@ -0,0 +1,39 @@
+import os
+import requests
+# Import the Python SDK
+import google.generativeai as genai
+# Used to securely store your API key
+from dotenv import load_dotenv
+
+load_dotenv()
+GOOGLE_API_KEY = os.environ.get("GEMINI_PASS_KEY") 
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+def prompting(tweet):
+    emotions = ["ambiguous", "amusement", "anger", "anxiety", "belief", "confusion", "depression", "disgust", "excitement", "optimism", "panic", "surprise"]
+    
+    prompt = (
+        f"You are an intelligent stock based tweet analyzing assistant that selects out of 12 emotions, the one that best represents the feeling towards a certain company's stock. Based on the user's tweet: '{tweet}', "
+        f"select the most relevant emotion, only one singular, from the following list {emotions}. Also say whether the tweet represents a bullish or bearish market for that specific company\n\n"
+        f"your response should only contain two words, the emotion and if its either bullish or bearish (market sentiment)"
+    )
+
+
+    model = genai.GenerativeModel('gemini-2.0-flash')
+    response = model.generate_content(prompt)
+    categorizing = response.candidates[0].content.parts[0].text.strip()
+
+    return categorizing
+
+def start_chat():
+    while True:
+        user_query = input("\nEnter your tweet: ")
+        if user_query.lower() == "exit":
+            print("Goodbye!")
+            break
+        response = prompting(user_query)       
+        response_array = response.split() 
+        print(response_array)
+
+start_chat()
