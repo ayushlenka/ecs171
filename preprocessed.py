b35e40985bd85512ad3723cf967d6201177c4da1
Justin Chang <chang.justin0106@gmail.com>
2025-03-05 22:50:48 -0800

    Cleanup

--- preprocessed.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-for company in ['AMZN', 'AAPL', 'MSFT', 'TSLA']:
-    df = pd.read_csv(f"{company}.csv")
-
-    df["senti_label"] = df["senti_label"] == "bullish"
-    df.to_csv(f"{company}.csv")
