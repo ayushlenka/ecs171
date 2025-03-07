2861c6013e42211f2e8204b32a491bf75a880628
Justin Chang <chang.justin0106@gmail.com>
2025-03-06 18:23:39 -0800

    Added training to LSTM

--- LSTM.py
+++ LSTM.py
@@ -1,6 +1,9 @@
-import torch.nn as nn
 import torch
-import os
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_layers, batch_size):
@@ -25,5 +28,44 @@ class LSTM(nn.Module):
     
 
 if __name__ == "__main__":
-    model = LSTM(2, 10, 1, 2, 10)
+    model = LSTM(13, 10, 1, 2, 10)
 
+    df = pd.read_csv('CSV_Files/AAPL.csv')
+
+    x = df.iloc[:, [3] + list(range(5, 17))]
+    y = df.iloc[:, 4]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8)
+
+    x_train_tensor = torch.tensor(x_train.values, dtype=torch.bool)
+    x_test_tensor = torch.tensor(x_test.values, dtype=torch.bool)
+    y_train_tensor = torch.tensor(y_train.values, dtype=torch.bool)
+    y_test_tensor = torch.tensor(y_test.values, dtype=torch.bool)
+
+    print(x_train_tensor.shape)
+
+    lossFunction = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.03)
+
+    num_epochs = 500
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        output = model(x_train_tensor)
+        loss = lossFunction(output, y_train_tensor)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (epoch + 1) % 50 == 0:
+            with torch.no_grad():
+                MSE = F.mse_loss(output, y_train_tensor)
+                print(f'Epoch {epoch + 1}: MSE = {MSE}')
+
+    model.eval()
+    with torch.no_grad():
+        y_pred = model(x_train_tensor)
+        MSE = F.mse_loss(y_pred, y_test_tensor)
+        print(f"Train MSE: {MSE}")
--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,11 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
-data = pd.read_csv("CSV_Files/merged_file.csv")
+# df = pd.read_csv("CSV_Files/AMZN.csv")
+
+# print(df.iloc[:,  [4] + list(range(6, 18))])
 
 for name in ['AMZN', 'AAPL', 'MSFT', 'TSLA']:
-    df = data[data["ticker"] == name]
-    df.drop(columns=["Open", "High", "Low", "Close", "Adj Close", "Volume", "processed"], inplace=True)
+    df = pd.read_csv(f'CSV_Files/{name}.csv')
 
-    df1 = pd.get_dummies(df["emo_label"], prefix="emo")
-    df = pd.concat([df, df1], axis=1)
-    df.drop(columns=["emo_label"], inplace=True)
-    df.reset_index(drop=True,inplace=True)
-
-    # print(df)
-
-    df.to_csv(f"CSV_Files/{name}.csv")
+    print(df.iloc[:, 4])
