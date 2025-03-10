c9280627e464b47e95a1905142be4a3943e2041f
ayushlenka <118499424+ayushlenka@users.noreply.github.com>
2025-03-09 22:34:05 -0700

    new branch

--- preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-
-df = pd.read_csv("CSV_Files/MSFT.csv")
-
-df.drop(columns=["id"], inplace=True)
-print(df)
-
-df.to_csv("CSV_Files/MSFT.csv", index=False)
